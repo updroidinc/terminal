a8194f3a676c3bdb4ce3ac5efda61a1f26d37e03
Mike Lewis <mike@updroid.com>
2015-04-19 12:06:26 -0700

    example: fixed I/O upon reconnect

--- example/index.dart
+++ example/index.dart
@@ -4,34 +4,39 @@ import 'dart:typed_data';
 import 'package:terminal/terminal.dart';
 import 'package:terminal/theme.dart';
 
+WebSocket ws;
+Terminal term;
+
 void main() {
-  Terminal term = new Terminal(querySelector('#console'))
+  term = new Terminal(querySelector('#console'))
     ..scrollSpeed = 3
     ..cursorBlink = true
     ..theme = new Theme.SolarizedDark();
 
-  WebSocket ws = initWebSocket('ws://localhost:12061/pty');
+  initWebSocket('ws://localhost:12061/pty');
 
   term.stdin.stream.listen((data) {
-    print(data.toString());
     ws.sendByteBuffer(new Uint8List.fromList(data).buffer);
   });
-
-  ws.onMessage.listen((e) {
-    ByteBuffer buf = e.data;
-    term.stdout.add(buf.asUint8List());
-  });
-
 }
 
-WebSocket initWebSocket(String url, [int retrySeconds = 2]) {
+void initWebSocket(String url, [int retrySeconds = 2]) {
     bool encounteredError = false;
 
-    WebSocket ws = new WebSocket(url);
+    ws = new WebSocket(url);
     ws.binaryType = "arraybuffer";
 
+    ws.onOpen.listen((e) {
+      print('Console-$num connected to server with status ${ws.readyState}.');
+    });
+
+    ws.onMessage.listen((e) {
+      ByteBuffer buf = e.data;
+      term.stdout.add(buf.asUint8List());
+    });
+
     ws.onClose.listen((e) {
-      print('Console-$num disconnected. Retrying...');
+      print('Console-$num disconnected due to CLOSE. Retrying...');
       if (!encounteredError) {
         new Timer(new Duration(seconds:retrySeconds), () => initWebSocket(url, retrySeconds * 2));
       }
@@ -39,12 +44,10 @@ WebSocket initWebSocket(String url, [int retrySeconds = 2]) {
     });
 
     ws.onError.listen((e) {
-      print('Console-$num disconnected. Retrying...');
+      print('Console-$num disconnected due to ERROR. Retrying...');
       if (!encounteredError) {
         new Timer(new Duration(seconds:retrySeconds), () => initWebSocket(url, retrySeconds * 2));
       }
       encounteredError = true;
     });
-
-    return ws;
   }
